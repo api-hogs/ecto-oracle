8a33ab868463ff5e7e5f5deb4005aab052ec9a85
Opak Alex <opak.alexandr@gmail.com>
2016-01-01 21:45:52 +0200

    add deps for OCI

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule EctoOracleAdapter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:erloci, git: "git://github.com/K2InformaticsGmbH/erloci.git"}]
   end
 end
