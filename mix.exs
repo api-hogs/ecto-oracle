919164ade6e0f88f1ada3067bcf5425ab236cbae
Opak Alex <opak.alexandr@gmail.com>
2016-01-03 13:07:01 +0200

    add ecto

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule EctoOracleAdapter.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :ecto, :erloci]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,7 @@ defmodule EctoOracleAdapter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:erloci, git: "git://github.com/K2InformaticsGmbH/erloci.git"}]
+    [{:erloci, git: "git://github.com/K2InformaticsGmbH/erloci.git"},
+      {:ecto, "~> 1.1"}]
   end
 end
