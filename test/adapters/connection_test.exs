b092ee03a93b72c5cbad41990f2251bfe739a40d
iggor_kurr <kurinnoyi@gmail.com>
2016-01-04 16:39:38 +0200

    fix conventions

--- test/adapters/connection_test.exs
+++ test/adapters/connection_test.exs
@@ -6,16 +6,16 @@ defmodule EctoOracleAdapterTest do
   @user "system"
   @password "oracle"
   @tns "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=tcp)(HOST=46.101.228.77)(PORT= 49161)))(CONNECT_DATA=(SERVICE_NAME=XE)))"
-  @credentials %{ :tns => @tns, :user => @user, :password => @password}
+  @credentials %{ tns: @tns, user: @user, password: @password}
 
   test "connect" do
-    {:ok, conn} = Connection.connect(%{ :credentials => @credentials })
+    {:ok, conn} = Connection.connect(%{ credentials: @credentials })
 
     assert conn
   end
 
   test "disconnect" do
-    {:ok, conn} = Connection.connect(%{ :credentials => @credentials })
+    {:ok, conn} = Connection.connect(%{ credentials: @credentials })
 
     assert Connection.disconnect(conn)
   end
