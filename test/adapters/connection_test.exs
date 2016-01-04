731f952ef6e4f5b898c4e5d160c7ed0cdf2da069
iggor_kurr <kurinnoyi@gmail.com>
2016-01-04 16:04:55 +0200

    add connection basic tests

---
+++ test/adapters/connection_test.exs
@@ -0,0 +1,23 @@
+defmodule EctoOracleAdapterTest do
+  use ExUnit.Case, async: true
+
+  alias EctoOracleAdapter.Connection
+
+  @user "system"
+  @password "oracle"
+  @tns "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=tcp)(HOST=46.101.228.77)(PORT= 49161)))(CONNECT_DATA=(SERVICE_NAME=XE)))"
+  @credentials %{ :tns => @tns, :user => @user, :password => @password}
+
+  test "connect" do
+    {:ok, conn} = Connection.connect(%{ :credentials => @credentials })
+
+    assert conn
+  end
+
+  test "disconnect" do
+    {:ok, conn} = Connection.connect(%{ :credentials => @credentials })
+
+    assert Connection.disconnect(conn)
+  end
+
+end
