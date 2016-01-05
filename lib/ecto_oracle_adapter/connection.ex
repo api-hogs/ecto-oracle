7f2092ab3f951f5b41c9a9414ce1ee5d4960d1cd
iggor_kurr <kurinnoyi@gmail.com>
2016-01-05 17:46:43 +0200

    use timestamp datetype

--- lib/ecto_oracle_adapter/connection.ex
+++ lib/ecto_oracle_adapter/connection.ex
@@ -789,7 +789,7 @@ defmodule EctoOracleAdapter.Connection do
   defp ecto_to_db(:id),         do: "integer"
   defp ecto_to_db(:binary_id),  do: "uuid"
   defp ecto_to_db(:string),     do: "varchar"
-  defp ecto_to_db(:datetime),   do: "DATE"
+  defp ecto_to_db(:datetime),   do: "TIMESTAMP"
   defp ecto_to_db(:binary),     do: "bytea"
   defp ecto_to_db(:map),        do: "jsonb"
   defp ecto_to_db(:bigint),     do: "NUMBER"
